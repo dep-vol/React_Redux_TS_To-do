d809a995ba4ca4a5bd9e791e41b86fa97639b933
tecto <dep-vol@mail.ru>
2020-04-17 16:00:49 +0300

    Project init

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./Header.scss";
+
+const Header = () => {
+    return (
+        <header className={style.header}>
+            TO-DO REDUX APP
+        </header>
+    )
+};
+
+export default Header;
