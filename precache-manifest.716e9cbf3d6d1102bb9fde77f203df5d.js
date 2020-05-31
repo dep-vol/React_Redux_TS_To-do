4a597bbd6b1601ac647a927c016bbc3a2ae98372
dep-vol <alex.tecto.msk@gmail.com>
2020-05-31 07:29:14 +0300

    Updates

---
+++ precache-manifest.716e9cbf3d6d1102bb9fde77f203df5d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c87a61478ee434fe72042eb95c07f4",
+    "url": "/React_Redux_TS_To-do/index.html"
+  },
+  {
+    "revision": "142a6afde4e4346f97ea",
+    "url": "/React_Redux_TS_To-do/static/css/3.0703f7b0.chunk.css"
+  },
+  {
+    "revision": "f8d9b0bdba2e941f41de",
+    "url": "/React_Redux_TS_To-do/static/css/main.ab6e647e.chunk.css"
+  },
+  {
+    "revision": "f97e0d30fb165de22a4a",
+    "url": "/React_Redux_TS_To-do/static/js/2.8e153f3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Redux_TS_To-do/static/js/2.8e153f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142a6afde4e4346f97ea",
+    "url": "/React_Redux_TS_To-do/static/js/3.1e92fec7.chunk.js"
+  },
+  {
+    "revision": "f8d9b0bdba2e941f41de",
+    "url": "/React_Redux_TS_To-do/static/js/main.06db80da.chunk.js"
+  },
+  {
+    "revision": "bcf2a75bb8591923f035",
+    "url": "/React_Redux_TS_To-do/static/js/runtime-main.e9544f02.js"
+  },
+  {
+    "revision": "85772647236e76b8e772c14c092a28ac",
+    "url": "/React_Redux_TS_To-do/static/media/redux.85772647.svg"
+  }
+]);
